56dc853a94a38fa9e399885a3381ee9224102d62
Yash-1485 <yashparekh140805@gmail.com>
2025-01-05 00:04:19 +0530

    Added JavaScript Programs

---
+++ JS/Condition_Statements.js
@@ -0,0 +1,16 @@
+// console.log("Hello World!")
+// let age=17;
+
+// if (age>18){
+//     console.log("You can drive at this age.")
+// }else{
+//     console.log("You can't drive at this age.")
+// }
+
+// console.log("3"==3)
+// console.log("3"==3.0)
+// console.log("3"==="3.0")
+// console.log("3"!==3)
+console.log("3"!=="3")
+
+// REPL-Read Evaluate Print Loop
---
+++ JS/Variables_Data_Types.js
@@ -0,0 +1,47 @@
+// console.log("Hello World!");
+
+// let a="Yash";
+// let b=100;
+// let c=10000000000000000000000n;// for big int, we have to type n after number
+// let d=undefined;
+// let e=null;//type of null is object
+// let f=3.14;
+// let g=true;
+// console.log(a,b,c,d,e,f,g);
+// console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g);
+
+// let v/s var
+// let a=30;
+// if(a>=50){
+//     console.log(a);
+// }else{
+//     let a=45;
+//     console.log(a);
+// }
+// console.log(a);
+
+// var a=30;
+// if(a>=50){
+//     console.log(a);
+// }else{
+//     var a=45;
+//     console.log(a);
+// }
+// console.log(a);
+
+// const
+// const a=90;
+// console.log(a,typeof a);
+
+// a=91;
+// console.log(a);
+
+// Object
+// let obj=new Object();
+// obj.name="Yash";
+// obj.rn=3;
+// obj.marks=100;
+// console.log(obj.rn,obj.name,obj.marks,typeof obj);
+
+let obj={rn:3,name:"Yash",marks:100};
+console.log(obj.rn,obj.name,obj.marks,typeof obj);
