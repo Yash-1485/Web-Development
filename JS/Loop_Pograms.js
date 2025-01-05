5ed0910a4f94a84af2d48db202dfed9bdd022f51
Yash1485 <yashparekh140805@gmail.com>
2025-01-05 16:05:28 +0530

    Added Loops and Loop programs

---
+++ JS/Loop_Pograms.js
@@ -0,0 +1,67 @@
+// let n=5
+// for(let i=1;i<=n;i++){
+//     console.log('*'.repeat(i));
+// }
+
+// let x=10;
+// let y=10;
+
+// let str3 = "";
+
+// for(let i = 1; i < y; i++ ){
+//     for(let j = 1; j < x; j++){
+//         if(i >= j ){
+//             str3 = str3.concat("*");
+//         }
+//     }
+//     str3 = str3.concat("\n")
+// }
+
+// console.log(str3)
+
+let n=5
+// for(let i=1;i<=n;i++){
+//     let st=""
+//     for(let j=1;j<=i;j++){
+//         st=st.concat("*")
+//     }
+//     console.log(st)
+// }
+
+// for(let i=1;i<=n;i++){
+//     let st=""
+//     for(let j=n;j>=i;j--){
+//         st=st.concat("*")
+//     }
+//     console.log(st)
+// }
+
+// for(let i=1;i<=n;i++){
+//     let st=""
+//     for(let k=1;k<i;k++){
+//         st=st.concat(" ")
+//     }
+//     for(let j=n;j>=i;j--){
+//         st=st.concat("*")
+//     }
+//     console.log(st)
+// }
+
+// for(let i=1;i<=n;i++){
+//     let st=""
+//     for(let k=n;k>=i;k--){
+//         st=st.concat(" ")
+//     }
+//     for(let j=1;j<=i;j++){
+//         st=st.concat("*")
+//     }
+//     console.log(st)
+// }
+
+// for(let i=1;i<=n;i++){
+//     let st=""
+//     for(let j=1;j<=i;j++){
+//         st=st.concat(String.fromCharCode(j+96))
+//     }
+//     console.log(st)
+// }
---
+++ JS/Loops.js
@@ -0,0 +1,44 @@
+// console.log("Hello World!");
+
+// Loops - for loop
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// while loop
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// Do while loop
+// let i=1;
+// do{
+//     console.log(i);
+//     i+=1;
+// }while(i<=5);
+
+// foreach loop
+// [1,2,3,4,5].forEach(element => {
+//     console.log(element);
+// });
+
+// for in loop
+// let obj={
+//     rn:3,
+//     name:"Yash",
+//     marks:100,
+// }
+// for(key in obj){
+//     console.log("Key:",key,"Value:",obj[key]);
+// }
+
+// for of loop
+st="Yash";//index
+// for(ele in st){
+//     console.log(st[ele]);
+// }
+for(ele of st){
+    console.log(ele);
+}
