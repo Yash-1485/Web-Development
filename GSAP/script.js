6f87fc2fa5e670d2b670f615b898a03b6b0cb81d
Yash1485 <yashparekh140805@gmail.com>
2025-01-06 23:28:34 +0530

    GSAP Update

--- GSAP/script.js
+++ GSAP/script.js
@@ -42,11 +42,56 @@ gsap.from("#box2",{
 
 // yoyo: true/false -> sets alternate
 // repeat: no.of total repeats (+1), -1 for infinite
-gsap.to("#box",{
-    x:1200,
+// gsap.to("#box",{
+//     x:1200,
+//     duration:0.5,
+//     delay:1,
+//     rotate:360,
+//     repeat:3,
+//     yoyo:true
+// })
+
+// timeline() - to make objects appear one by one
+// let tl=gsap.timeline()
+
+// tl.to("#box1",{
+//     x:1500,
+//     duration:1,
+//     delay:0.5,
+//     rotate:360
+// })
+// tl.to("#box2",{
+//     x:1500,
+//     duration:1,
+//     scale:0.5,
+//     rotate:360
+// })
+// tl.to("#box3",{
+//     x:1500,
+//     duration:1,
+//     rotate:360,
+//     borderRadius:"50%"
+// })
+
+// Try
+let tl=gsap.timeline()
+tl.from(".nav>h2",{
+    y:-30,
+    duration:1,
+    delay:0.5,
+    opacity:0
+})
+
+tl.from(".nav-part>.nav-item",{
+    y:-30,
+    duration:1,
+    opacity:0,
+    stagger:0.3
+})
+
+tl.from(".main>h1",{
+    y:0,
+    opacity:0,
     duration:0.5,
-    delay:1,
-    rotate:360,
-    repeat:3,
-    yoyo:true
+    scale:0.2
 })
