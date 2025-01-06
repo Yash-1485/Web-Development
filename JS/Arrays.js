484ffb14c6a54b9c200d59f534cfe1fadca2a609
Yash-1485 <yashparekh140805@gmail.com>
2025-01-07 00:44:04 +0530

    Added Array JS file

---
+++ JS/Arrays.js
@@ -0,0 +1,51 @@
+// console.log("Hello World!")
+
+let arr=[1,2,3,4,5]
+// console.log(arr)
+// console.log(arr[4],arr[0],arr[5],typeof arr)
+
+// console.log(arr.length)
+// console.log(arr.push("Yash"),arr)
+// console.log(arr.push(100),arr)
+// console.log(arr.pop(),arr)
+// console.log(arr.shift(),arr)
+// console.log(arr.unshift(33),arr)
+
+// console.log(arr.toString(),typeof arr.toString())
+// console.log(arr.join(" and "))
+
+// console.log(Array.from("Man"))
+
+arr=[1,2,3,4,[5,6]]
+// console.log(arr.flat())
+
+// Loops
+// arr.forEach((value,idx,arr)=>{
+//     // console.log(value,idx,arr)
+//     console.log(idx,value)
+// })
+
+// for(ele of arr){
+//     console.log(ele)
+// }
+
+// for(idx in arr){
+//     console.log(arr[idx])
+// }
+
+// console.log(arr.pop())
+// console.log(arr.concat([5,6,7],[8,9,10]))
+arr=[1,2,3,4]
+
+// console.log(arr.reverse())
+
+// console.log(arr.sort())
+// console.log(arr.sort((x,y)=>{return y-x}))
+
+// Map, Filter and Reduce Functions
+// console.log(arr.map(String))
+// console.log(arr.map((x)=>x**2))
+
+// console.log(arr.filter((x)=>x%2!=0))
+
+console.log(arr.reduce((x,y)=>x*y))
