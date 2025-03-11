4cbaa5cfe01d16c189d9fc046a3964589a52743d
Yash1485 <yashparekh140805@gmail.com>
2025-03-11 10:26:09 +0530

    Added First React Based App

---
+++ ReactJS/my-first-react-app/README.md
@@ -0,0 +1,4 @@
+# Creating My First React APP
+
+## Source
+- CodeWithHarry: [text](https://youtu.be/RGKi6LSPDLU?si=KJ9IR5CUCZQfwqkq)
---
+++ ReactJS/my-first-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './MyComponents/Header';
+import Todos from './MyComponents/Todos';
+import Footer from './MyComponents/Footer';
+
+function App() {
+  return (
+    <>
+      <Header title="My Todos List" searchBar={true}/>
+      <Todos/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
